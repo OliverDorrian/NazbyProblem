e2c510b2bf1c335de83243b99fe25dbb6da153f2
OliverDorrian <dorrianolfc@gmail.com>
2023-10-29 19:29:30 +0000

    intit

--- coordReader.c
+++ coordReader.c
@@ -1,91 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<stdbool.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 
-/*This code is for reading and writing to files for the 2023-24 COMP528 CA1*/
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wwritable-strings"
+#pragma clang diagnostic pop
 
-/*Use the functions in this file to read from the input file, and write to the output file*/
-
-/*You should use this file when compiling your code*/
-
-/*Declare these functions at the top of each 'main' file*/
-
-/*If there are any issues with this code, please contact: h.j.forbes@liverpool.ac.uk*/
-
-int readNumOfCoords(char *fileName);
-double **readCoords(char *filename, int numOfCoords);
+int readNumOfCoords(const char *fileName);
+double **readCoords(const char *filename, int numOfCoords);
 
 /*Gets the number of the coordinates in the file. Returns as a single integer*/
-int readNumOfCoords(char *filename){
-	FILE *file = fopen(filename, "r");
-	int numOfCoords = 0;
+int readNumOfCoords(const char *filename){
+    FILE *file = fopen(filename, "r");
+    int numOfCoords = 0;
 
-	if(file == NULL){
-		return -1;
-	}
+    if(file == nullptr){
+        return -1;
+    }
 
-	char line[100];
+    char line[100];
 
-	while(fgets(line, sizeof(line), file) != NULL){
-		numOfCoords++;
-	}
-	
+    while(fgets(line, sizeof(line), file) != nullptr){
+        numOfCoords++;
+    }
+
     return numOfCoords;
 }
 
 /*Gets the data from the file. Returns as an array of doubles, Ignores the first integer*/
-double **readCoords(char *filename, int numOfCoords){
-	FILE *file = fopen(filename,"r");
+double **readCoords(const char *filename, int numOfCoords){
+    FILE *file = fopen(filename,"r");
     int i;
 
-	char line[100];
-    
-    if(file == NULL) {
+    char line[100];
+
+    if(file == nullptr) {
         printf("Unable to open file: %s", filename);
-        return NULL;
+        return nullptr;
     }
 
-	double **coords = (double **)malloc(numOfCoords * sizeof(double *));
+    double **coords = (double **)malloc(numOfCoords * sizeof(double *));
 
-	for(i = 0; i < numOfCoords; i++){
-		coords[i] = (double *) malloc(2 * sizeof(int));
-		if (coords[i] == NULL){
-			perror("Memory Allocation Failed");
-		}
-	}
+    for(i = 0; i < numOfCoords; i++){
+        coords[i] = (double *) malloc(2 * sizeof(double));
+        if (coords[i] == nullptr){
+            perror("Memory Allocation Failed");
+        }
+    }
 
-	int lineNum = 0;
-	while(fgets(line, sizeof(line), file) != NULL){
-		double x, y;
-		if (sscanf(line, "%lf,%lf", &x, &y) == 2){
-			coords[lineNum][0] = x;
-			coords[lineNum][1] = y;
-			lineNum++;
-		}
-	}
+    int lineNum = 0;
+    while(fgets(line, sizeof(line), file) != nullptr){
+        double x, y;
+        if (sscanf(line, "%lf,%lf", &x, &y) == 2){
+            coords[lineNum][0] = x;
+            coords[lineNum][1] = y;
+            lineNum++;
+        }
+    }
 
-	return coords;
+    return coords;
 }
 
 void *writeTourToFile(int *tour, int tourLength, char *filename){
-	
-	FILE *file = fopen(filename, "w");
-	int i;	
-	
-	if(file == NULL){
-		printf("Unable to open file: %s", filename);
-		return NULL;
-	}
 
-	fprintf(file, "%d \n", tourLength);
+    FILE *file = fopen(filename, "w");
+    int i;
 
-	printf("Writing output data\n");
+    if(file == nullptr){
+        printf("Unable to open file: %s", filename);
+        return nullptr;
+    }
+
+    fprintf(file, "%d \n", tourLength);
+
+    printf("Writing output data\n");
     for(i=0; i < tourLength; i++) {
         fprintf(file, "%d ", tour[i]);
     }
-
-	
-
+    return nullptr;
 }
